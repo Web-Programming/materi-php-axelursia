b8fda9fdf7dc24151fa3512ca8255231d6070e3d
Axel Theo Winata Ursia <axeltheowinataursia_2226240117@mhs.mdp.ac.id>
2025-05-03 18:33:21 +0700

    add : fakultas layout

--- latihan_php/routes/web.php
+++ latihan_php/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\ProdiController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+route::get('/prodi/index', [ProdiController::class, 'index']);
